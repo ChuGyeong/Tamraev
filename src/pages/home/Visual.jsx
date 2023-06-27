9e462f889acf9c93f5f5c8831049ae0b0b657624
honeypunch97 <bkws04211@naver.com>
2023-06-27 15:37:06 +0900

    fix: folder location

---
+++ src/pages/home/Visual.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Visual = memo(() => {
+   return (
+      <div>
+         <img src="./images/common/visual.jpg" alt="" />
+      </div>
+   );
+});
+
+export default Visual;
