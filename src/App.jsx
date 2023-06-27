8462812ac1dce65af21ee0a034274545c525b0a1
ChuGyeong <unkns@naver.com>
2023-06-27 16:09:24 +0900

    add: router to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Info from './pages/info/Info';
 import NoticeList from './pages/noticeList/NoticeList';
 import Home from './pages/home/Home';
-import Visual from './pages/home/Visual';
 
 const App = () => {
    return (
       <>
          <GlobalStyle />
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="info" element={<Info />} />
+               <Route path="NoticeList" element={<NoticeList />} />
+            </Routes>
+         </BrowserRouter>
       </>
    );
 };
